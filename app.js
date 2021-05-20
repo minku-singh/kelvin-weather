2e76f4cacc866e0d2b554a17a4cabd4490977a21
Minku Singh <mailminkusingh@gmail.com>
2021-05-20 23:22:25 +0530

    added JavaScript file

---
+++ app.js
@@ -0,0 +1,7 @@
+const kelvin = 293;
+let celcius = kelvin - 273;
+let farenheit = Math.floor(celsius * (9 / 5) + 32);
+
+console.log(`The temperature is ${kelvin} degrees Kelvin.`);
+console.log(`The temperature is ${celsius} degrees Celsius.`);
+console.log(`The temperature is ${farenheit} degrees Fahrenheit.`);
